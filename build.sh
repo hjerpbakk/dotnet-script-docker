32007cd332b47c7d17b1212898ae8f627544c2a3
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2021-09-21 20:58:14 +0200

    Added build and publish scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build -t hjerpbakk/dotnet-script:1.2.1 .
+docker build -t hjerpbakk/dotnet-script:latest .
---
+++ publish.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker push hjerpbakk/dotnet-script:1.2.1
+docker push hjerpbakk/dotnet-script:latest
